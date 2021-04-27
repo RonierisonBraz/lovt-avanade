47e67d44954044dfd3928d10134a75d43c3c1e38
RonierisonBraz <ronierison18@gmail.com>
2021-04-27 11:48:56 -0300

    começando desafio de validação

--- js/main.js
+++ js/main.js
@@ -21,17 +21,31 @@ const toggleMode = () => {
     document.body.classList.toggle("dark-mode");
 }
 
-document.querySelector("#btn-submit-contact").addEventListener("click", (evento) => {
-    evento.preventDefault(); //cancelando o envio temporariamente
+// document.querySelector("#btn-submit-contact").addEventListener("click", (evento) => {
+//     evento.preventDefault(); //cancelando o envio temporariamente
 
 
-    let nomeValue = document.querySelector("#input_nome").value;
-    let emailValue = document.querySelector("#input_email").value;
+//     let nomeValue = document.querySelector("#input_nome").value;
+//     let emailValue = document.querySelector("#input_email").value;
 
-    console.log(nomeValue);
-    console.log(emailValue);
+//     console.log(nomeValue);
+//     console.log(emailValue);
 
-    //validação dos campos 
-    //exibição de mensagens de erros
-    //envio do formulário
+//     //validação dos campos 
+//     //exibição de mensagens de erros
+//     //envio do formulário
+// });
+
+let formContact = document.querySelector("form_contact");
+
+formContact.addEventListener("submit", (evento) => {
+    //interromp o envio do formuário 
+    evento.preventDefault();
+
+    //validar se todos os campos estão preenchido
+    //validar se o nome tem 2 ou mais caracteres
+    //validar se teefone tem no minimo 8 caracteres
+    //validar se o campo email tem @ (Google - validação com Regex)
+
 })
+
