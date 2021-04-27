bdca1d2e4d2a0689399ce9180a3e409a593db608
RonierisonBraz <ronierison18@gmail.com>
2021-04-27 11:24:35 -0300

    testando formulario no console

--- js/main.js
+++ js/main.js
@@ -20,3 +20,18 @@ logo.style.color = "orange";
 const toggleMode = () => {
     document.body.classList.toggle("dark-mode");
 }
+
+document.querySelector("#btn-submit-contact").addEventListener("click", (evento) => {
+    evento.preventDefault(); //cancelando o envio temporariamente
+
+
+    let nomeValue = document.querySelector("#input_nome").value;
+    let emailValue = document.querySelector("#input_email").value;
+
+    console.log(nomeValue);
+    console.log(emailValue);
+
+    //validação dos campos 
+    //exibição de mensagens de erros
+    //envio do formulário
+})
